915efe908162d7dabb4016379c7f806839da30ca
Kaustav-Bhattacharya <bhattacharyar64@gmail.com>
2024-08-05 09:18:20 +0530

    fix: description page

--- app/task-detail/[taskId]/page.tsx
+++ app/task-detail/[taskId]/page.tsx
@@ -1,5 +1,3 @@
-//@ts-nocheck
-
 "use client";
 import Header from "@/components/custom/header";
 import { getPriorityColor } from "@/components/custom/swipable-card";
@@ -28,24 +26,39 @@ const TaskDetail = (props: Props) => {
   const dispatch = useDispatch();
 
   const handleEdit = () => {
-    router.push(`/${taskToDisplay?.id}`);
+    if (taskToDisplay) {
+      router.push(`/${taskToDisplay.id}`);
+    }
   };
 
   const handleRemove = () => {
-    dispatch(removeTask(taskToDisplay?.id));
-    toast({
-      title: "Task Removed.",
-      variant: "destructive",
-    });
+    if (taskToDisplay) {
+      dispatch(removeTask(taskToDisplay.id));
+      router.back();
+      toast({
+        title: "Task Removed.",
+        variant: "destructive",
+      });
+    }
   };
 
   const handleToggleComplete = () => {
-    dispatch(toggleTaskCompletion(taskToDisplay.id));
-    const title = taskToDisplay?.completed ? "Task Marked Incomplete." : "Task Marked Completed";
-    toast({
-      title,
-    });
+    if (taskToDisplay) {
+      dispatch(toggleTaskCompletion(taskToDisplay.id));
+      const title = taskToDisplay.completed
+        ? "Task Marked Incomplete."
+        : "Task Marked Completed";
+      toast({
+        title,
+      });
+    }
   };
+
+  if (!taskToDisplay) {
+    // Redirect or display a message if the task does not exist
+    router.push("/task-list"); // Redirect to a safe page
+    return null;
+  }
 
   return (
     <div className="container max-w-[768px]">
@@ -54,12 +67,12 @@ const TaskDetail = (props: Props) => {
         className={cn(
           "rounded shadow-md p-4 min-h-[60vh] bg-white dark:bg-gray-800",
           {
-            "text-gray-600 dark:text-gray-400": taskToDisplay?.completed,
+            "text-gray-600 dark:text-gray-400": taskToDisplay.completed,
           }
         )}
       >
         <h2 className="text-xl font-bold flex items-center gap-3 w-full justify-between mt-3 mb-1 text-gray-900 dark:text-gray-100">
-          {taskToDisplay?.title}
+          {taskToDisplay.title}
           <span className="flex gap-3 items-center">
             <Pen
               size={15}
@@ -75,20 +88,26 @@ const TaskDetail = (props: Props) => {
           </span>
         </h2>
         <p className="font-light italic text-gray-700 dark:text-gray-300">
-          {taskToDisplay?.description}
+          {taskToDisplay.description}
         </p>
         <div className="flex items-center justify-between mt-8 flex-wrap space-y-4">
           <p className="font-medium italic text-sm flex items-center text-nowrap flex-wrap text-gray-700 dark:text-gray-300">
-            Due: {format(new Date(taskToDisplay.dueDate), "EEEE, MMM dd, yyyy")}
+            Due:{" "}
+            {format(
+              new Date(taskToDisplay.dueDate || "2024-08-05T03:36:09.197Z"),
+              "EEEE, MMM dd, yyyy"
+            )}
             <Badge
               className={cn(
-                `${getPriorityColor(taskToDisplay?.priority)} md:ml-4 mr-1 text-nowrap text-white`,
+                `${getPriorityColor(
+                  taskToDisplay.priority
+                )} md:ml-4 mr-1 text-nowrap text-white`,
                 {
-                  "bg-gray-500 transition-colors": taskToDisplay?.completed,
+                  "bg-gray-500 transition-colors": taskToDisplay.completed,
                 }
               )}
             >
-              {taskToDisplay?.priority} on priority
+              {taskToDisplay.priority} on priority
             </Badge>
             <span className="text-nowrap hidden lg:block">(no pressure)*</span>
           </p>
@@ -97,21 +116,25 @@ const TaskDetail = (props: Props) => {
             <Checkbox
               className="size-5 transition-all ease-in"
               onCheckedChange={handleToggleComplete}
-              checked={taskToDisplay?.completed}
+              checked={taskToDisplay.completed}
             />
           </p>
         </div>
-        <div className="mt-4">
-          <MapContainer
-            center={[taskToDisplay.lat, taskToDisplay.lng]}
-            zoom={13}
-            style={{ height: "300px", width: "100%", position: "relative" }}
-            className="rounded-md"
-          >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[taskToDisplay.lat, taskToDisplay.lng]} />
-          </MapContainer>
-        </div>
+        {taskToDisplay.lat && taskToDisplay.lng && (
+          <div className="mt-4">
+            <MapContainer
+              center={[taskToDisplay.lat, taskToDisplay.lng]}
+              zoom={13}
+              style={{ height: "300px", width: "100%", position: "relative" }}
+              className="rounded-md"
+            >
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker
+                position={[taskToDisplay.lat, taskToDisplay.lng]}
+              />
+            </MapContainer>
+          </div>
+        )}
       </Card>
     </div>
   );
